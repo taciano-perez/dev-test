44fd49597781c6d1702a0ff383658d0fb82e6bc9
Taciano Perez <taciano.perez@hp.com>
2016-10-23 23:53:45 -0200

    Create BusStation.java

---
+++ src/main/java/goeuro/bus/direct/BusStation.java
@@ -0,0 +1,47 @@
+package goeuro.bus.direct;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A bus station. It has an ID and a set of bus routes that connect to it.
+ * @author Taciano Perez
+ *
+ */
+public class BusStation {
+	
+	/**
+	 * Station ID.
+	 */
+	private int stationID;
+
+	/**
+	 * Set of routes connecting to this station.
+	 */
+	private Set<Integer> routeIDs = new HashSet<Integer>();
+
+	/**
+	 * Constructs a new BusStation.
+	 * @param id the station id
+	 */
+	public BusStation(Integer id) {
+		this.stationID = id;
+	}
+
+	public int getStationID() {
+		return stationID;
+	}
+
+	public void setStationID(int stationID) {
+		this.stationID = stationID;
+	}
+
+	public Set<Integer> getRouteIDs() {
+		return routeIDs;
+	}
+
+	public void addRouteID(Integer routeID) {
+		this.routeIDs.add(routeID);
+	}
+
+}
