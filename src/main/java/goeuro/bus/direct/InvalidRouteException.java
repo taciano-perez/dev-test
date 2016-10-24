a4de696a0c86bca6916cb773b31d07997985cce5
Taciano Perez <taciano.perez@hp.com>
2016-10-23 23:54:50 -0200

    Create InvalidRouteException.java

---
+++ src/main/java/goeuro/bus/direct/InvalidRouteException.java
@@ -0,0 +1,16 @@
+package goeuro.bus.direct;
+
+/**
+ * Exception thrown when parsing an invalid route entry. 
+ * @author Taciano Perez
+ *
+ */
+public class InvalidRouteException extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+
+	public InvalidRouteException(String msg) {
+		super(msg);
+	}
+
+}
